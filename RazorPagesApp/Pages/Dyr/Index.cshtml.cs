a1ca194d1a512f9ce5f532accf6b1de2214cb5fd
ayshaaa1234 <aygupta04@gmail.com>
2025-05-26 09:19:32 +0200

    Opdateret navigation i layoutet ved at ændre menupunkter for at forbedre brugervenligheden. Ændret link til "DYR" og tilføjet "MEDLEMSKAB" i stedet for tidligere punkter. Justeret både desktop- og mobilmenuer for konsistens.

---
+++ RazorPagesApp/Pages/Dyr/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesApp.Pages.Dyr
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+} 
